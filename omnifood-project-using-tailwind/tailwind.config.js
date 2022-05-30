b192a0e41b0476afe9fd21aa5cc51c577fe9f049
LokeshJawale1996 <105481574+LokeshJawale1996@users.noreply.github.com>
2022-05-30 13:01:42 +0530

    Add files via upload
    omnifood project using tailwind

---
+++ omnifood-project-using-tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-orange": "#fdf2e9",
+        "light-grey": "#888",
+      },
+    },
+  },
+  plugins: [],
+};
